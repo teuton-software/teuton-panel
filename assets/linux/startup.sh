1815f776afe279534e11578592805032de7b0fca
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2019-02-19 13:21:24 +0000

    U fix run as root on linux

--- assets/linux/startup.sh
+++ assets/linux/startup.sh
@@ -2,4 +2,4 @@
 SCRIPTPATH="$(dirname "$(readlink -f "$0")")"
 JAVA=$SCRIPTPATH/jre/bin/java
 TEUTONPATH=$SCRIPTPATH/teuton-panel
-pkexec /bin/bash -c DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY && $JAVA -jar $TEUTONPATH $@
+pkexec $JAVA -jar $TEUTONPATH $@
