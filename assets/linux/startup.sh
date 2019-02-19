85d76b0f1f2038df7aa711840ecb5cfc8dfd2ff1
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2019-02-19 11:59:08 +0000

    U fix startup.sh script to run app as root

--- assets/linux/startup.sh
+++ assets/linux/startup.sh
@@ -2,4 +2,4 @@
 SCRIPTPATH="$(dirname "$(readlink -f "$0")")"
 JAVA=$SCRIPTPATH/jre/bin/java
 TEUTONPATH=$SCRIPTPATH/teuton-panel
-pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY "$JAVA -jar $TEUTONPATH $@"
+pkexec /bin/bash -c DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY && $JAVA -jar $TEUTONPATH $@
