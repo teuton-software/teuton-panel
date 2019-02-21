d69d9d081a075a8669eb66ba8ab290f561610fa8
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2019-02-21 17:18:42 +0000

    Update deb-to-rpm.sh

--- assets/linux/deb-to-rpm.sh
+++ assets/linux/deb-to-rpm.sh
@@ -1,19 +1,18 @@
+  GNU nano 2.9.3                                            assets/linux/deb-to-rpm.sh                                                        
+
 #!/bin/bash
 
 function exist_command() {
-	[ ! $(which $1) ] && echo "$1 is needed to generate rpm from deb package" && exit 1  
+        [ ! $(which $1) ] && echo "$1 is needed to generate rpm from deb package" && exit 1  
 }
 
 exist_command alien
 exist_command rpmbuild
 
-filename=$1
-basename=$(basename -- $1)
-package="${file%.*}"
+filename=$(basename -- $1)
+package=$(echo "${filename%.*}" | tr "_" "-")
+dir=$(dirname -- $1)
 
-echo $filename
-echo $basename
-echo $package
-
-alien -g --to-rpm $file
-rpmbuild --buildroot $basename/$package --nodeps -bb $package.spec
+cd $dir
+alien -g --to-rpm $filename
+rpmbuild --buildroot $(pwd)/$package --nodeps -bb $package/*.spec
